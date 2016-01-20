18e80e6c5dac689c6fe4f6e6017b2e03322f9a1b
Glanzer <sglanzer@onm-sglanzer-01.ciena.com>
2016-01-20 15:08:06 -0500

    Cleaning build and dependencies

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,25 +1,12 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-var Funnel = require('broccoli-funnel');
-var flatiron = require("broccoli-flatiron");
-var path = require('path');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
 
-  var mdFunnel = new Funnel('markdown', {
-    include: [new RegExp(/\.md/)]
-  });
-  var mdFlattened = flatiron(mdFunnel, {
-      outputFile: 'assets/md.js',
-      trimExtensions: true
-  });
-  
-  app.import(mdFlattened);
-  
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
@@ -33,5 +20,5 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree(mdFlattened);
+  return app.toTree();
 };
